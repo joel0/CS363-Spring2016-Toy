df3600822d035f959d3466714d606d0dfc869a0f
Joel May <joel.may@live.com>
2016-04-24 15:41:16 -0500

    Header command and exception handling

--- Toy/Toy.vb
+++ Toy/Toy.vb
@@ -8,34 +8,38 @@
             Return
         End If
 
-        If args(0).Equals("create", StringComparison.CurrentCultureIgnoreCase) Then
-            createTableInteractive(args(1))
-        ElseIf args(0).Equals("header") Then
-            showHeader(args(1))
-        ElseIf args(0).Equals("insert", StringComparison.CurrentCultureIgnoreCase) Then
-            insertInteractive(args(1))
-        Else
-            If args.Count >= 3 Then
-                If args(0).Equals("display", StringComparison.CurrentCultureIgnoreCase) Then
-                    Dim rid As Integer
-                    If Not Integer.TryParse(args(1), rid) Then
-                        Throw New Exception("rid must be an integer")
-                    End If
-                    displayRecord(args(2), rid)
-                ElseIf args(0).Equals("delete", StringComparison.CurrentCultureIgnoreCase) Then
-                    Dim rid As Integer
-                    If Not Integer.TryParse(args(1), rid) Then
-                        Throw New Exception("rid must be an integer")
-                    End If
-                    deleteRecord(args(2), rid)
-                ElseIf args(0).Equals("search", StringComparison.CurrentCultureIgnoreCase) Then
-                    search(args(2), args(1))
-                End If
+        Try
+            If args(0).Equals("create", StringComparison.CurrentCultureIgnoreCase) Then
+                createTableInteractive(args(1))
+            ElseIf args(0).Equals("header") Then
+                showHeader(args(1))
+            ElseIf args(0).Equals("insert", StringComparison.CurrentCultureIgnoreCase) Then
+                insertInteractive(args(1))
             Else
-                showUsage()
-                Return
+                If args.Count >= 3 Then
+                    If args(0).Equals("display", StringComparison.CurrentCultureIgnoreCase) Then
+                        Dim rid As Integer
+                        If Not Integer.TryParse(args(1), rid) Then
+                            Throw New Exception("rid must be an integer")
+                        End If
+                        displayRecord(args(2), rid)
+                    ElseIf args(0).Equals("delete", StringComparison.CurrentCultureIgnoreCase) Then
+                        Dim rid As Integer
+                        If Not Integer.TryParse(args(1), rid) Then
+                            Throw New Exception("rid must be an integer")
+                        End If
+                        deleteRecord(args(2), rid)
+                    ElseIf args(0).Equals("search", StringComparison.CurrentCultureIgnoreCase) Then
+                        search(args(2), args(1))
+                    End If
+                Else
+                    showUsage()
+                    Return
+                End If
             End If
-        End If
+        Catch ex As Exception
+            Console.WriteLine(ex.Message)
+        End Try
 
         Console.ReadKey()
     End Sub
@@ -49,7 +53,7 @@
     End Sub
 
     Private Sub showHeader(fileName As String)
-
+        Console.WriteLine(Table.getHeaderFromFile(fileName))
     End Sub
 
     Private Sub insertInteractive(fileName As String)
